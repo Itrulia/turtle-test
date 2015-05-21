a2b8808c67981ab6064c7ceb78a6de40dd335806
Karl Merkli <info@karl-merkli.ch>
2015-05-21 14:28:41 +0200

    Moved algorithm to controller

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,61 @@
+<?php namespace TurtleTest\Http\Controllers;
+
+use TurtleTest\Services\Gateway\CupsInterface;
+
+class IndexController extends Controller {
+
+	/**
+	 * @param \TurtleTest\Services\Gateway\CupsInterface $aCupGateway
+	 * @return array
+	 */
+	public function index(CupsInterface $aCupGateway) {
+		$cups = $aCupGateway->getCups();
+
+		$teamSizes = new \Illuminate\Support\Collection();
+
+		foreach($cups as $cup) {
+			/**
+			 * @var \TurtleTest\Cup $cup
+			 * @var \TurtleTest\Bracket $bracket
+			 */
+			$bracket = $teamSizes->filter(function(\TurtleTest\Bracket $item) use ($cup) {
+				return $item->teamSize == $cup->teamSize;
+			})->first();
+
+			if (empty($bracket)) {
+				$bracket = new \TurtleTest\Bracket();
+				$bracket->teamSize = $cup->teamSize;
+
+				$teamSizes->push($bracket);
+			}
+
+			if (is_null($cup->winner)) {
+				continue;
+			}
+
+			/**
+			 * @var \TurtleTest\Winner $winner
+			 */
+			$winner = $bracket->winners->filter(function(\TurtleTest\Winner $item) use ($cup) {
+				return $item->id === $cup->winner->id;
+			})->first();
+
+			if (empty($winner)) {
+				$winner = new \TurtleTest\Winner();
+				$winner->id = $cup->winner->id;
+				$winner->wins = 0;
+
+				$bracket->winners->push($winner);
+			}
+
+			$winner->wins++;
+		}
+
+		$teamSizes->sortBy(function(\TurtleTest\Bracket $item) {
+			return $item->teamSize;
+		});
+
+
+		return $teamSizes->toArray();
+	}
+}
